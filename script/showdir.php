d5334c1e2179006339fb9272264da0316326d10a
FreeAlchemist <freealchemist@mail.ru>
2016-01-21 18:47:45 +0300

    Update showdir.php

--- script/showdir.php
+++ script/showdir.php
@@ -1,4 +1,20 @@
 <?php
+/*
+include("script/showdir.php");
+
+print_r(showpath("downloads"));
+
+$arr=showpath("downloads");
+foreach ($arr as $key => $value) {
+	echo "<li>
+	<span onclick=\"http_zapros('GET','downloads/".$value."')\">"
+	.$value.
+	"</span>"
+	."<a href=\"downloads/".$value."\"> скачать</a></li>";
+}
+
+*/
+
 function showpath($path,$depth=0,$foldernum=0){
 	$arr = scandir($path);
 	$result=array();
