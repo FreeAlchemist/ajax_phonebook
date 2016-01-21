e73659a2bbdfb7d015bd4bacfe8ec56b148861a2
FreeAlchemist <freealchemist@mail.ru>
2016-01-21 18:42:33 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,13 +4,15 @@
 <meta charset="utf-8">
 <link rel=Stylesheet href="css/ajax_phonebook.css">
 <script type="text/javascript" src="script/jquery/jquery-1.12.0.js"></script>
+<script type="text/javascript" src="script/classes.JSON.js"></script>
+
 </head>
 <body onload="postxml()">
 <div id="overlay">
-     <div>
+     <div id="dataform">
           <form>
           	<input type="reset">
-          	<table>
+          	<table class="editcard">
           		<tr>
           			<td>Фамилия</td>
           			<td>Имя</td>
@@ -34,15 +36,19 @@
 	          		<td><input type="text" id="city" placeholder="Ленинград"></td>
           		</tr>
 				</table>						
-          	<input type="submit" name="create" value="Сохранить контакт">
-          	<input type="submit" name="cancel" value="Отмена" formaction="../index.php">
+          	<input type="button" id="save" value="Сохранить контакт" onclick="savedata()">
+          	<input type="button" id="cancel" value="Отмена" onclick="closePopUp()">
+               <input type="button" id="delete" value="Удалить" onclick="savedata.delete()">
           </form>
+          </div>
+     <div id="loading">
+          <img src="img/loading.jpg">
      </div>
 </div>
+
 <div id="menu">
 <input type="submit" value="Скачать">
-<input type="submit" value="Добавить контакт">
-<hr>
+<input type="submit" value="Добавить контакт" onclick="openPopUp()">
 </div>
 <div id="cards"></div>
 
