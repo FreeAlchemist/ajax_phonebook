4fec18b8a283b7928b7466b08fc124c87e28730e
FreeAlchemist <freealchemist@mail.ru>
2016-01-21 18:48:45 +0300

    Update xml_json.php

--- script/xml_json.php
+++ script/xml_json.php
@@ -1,6 +1,9 @@
 <?php
+include("xml2json.php");
 $xmlstring = file_get_contents("../data/ajax_phonebook.xml");
-$xml = simplexml_load_string($xmlstring);
-$json = json_encode($xml);
+$json = xml2json::transformXmlStringToJson($xmlstring);
+
+// $xml = simplexml_load_string($xmlstring);
+// $json = json_encode($xml);
 echo $json;
 ?>
